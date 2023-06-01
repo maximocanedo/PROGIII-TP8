5b4f98c5a3381e822bcf7dd93a1de698f9bc3630
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-06-01 18:26:48 -0300

    Agregado de la clase TransactionResponse más referencias

---
+++ Entidades/TransactionResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Entidades {
+    public class TransactionResponse {
+        public int FilasAfectadas { get; set; }
+        public string Mensaje { get; set; }
+        public bool Estado { get; set; }
+        public object Resultado { get; set; }
+        public TransactionResponse(Conexion cn, int filasAfectadas = 0, object res = null) {
+            Mensaje = cn.DetalleError;
+            Estado = !cn.HuboError;
+            FilasAfectadas = filasAfectadas;
+            Resultado = res;
+        }
+    }
+}
