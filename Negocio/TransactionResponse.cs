3e912c395a5334fc7384de5db1b2070dec28a8fe
Ezequiel Martinez <EzequielMartinezA18@gmail.com>
2023-06-02 21:06:31 -0300

    orden de proyectos y clases hecho

--- Negocio/TransactionResponse.cs
+++ Negocio/TransactionResponse.cs
@@ -5,13 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Datos;
 
-namespace Entidades {
-    public class TransactionResponse {
+namespace Negocio
+{
+    public class TransactionResponse
+    {
         public int FilasAfectadas { get; set; }
         public string Mensaje { get; set; }
         public bool Estado { get; set; }
         public object Resultado { get; set; }
-        public TransactionResponse(Conexion cn, int filasAfectadas = 0, object res = null) {
+        public TransactionResponse(Conexion cn, int filasAfectadas = 0, object res = null)
+        {
             Mensaje = cn.DetalleError;
             Estado = !cn.HuboError;
             FilasAfectadas = filasAfectadas;
