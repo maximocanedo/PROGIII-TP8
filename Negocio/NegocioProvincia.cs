00628411ed1cd6092f5203bc50ae6f94e6d6f185
Oli-Hanczyc <oli.hanczyc@gmail.com>
2023-06-05 20:09:34 -0300

    Agregado del código de las clases de Negocio.

---
+++ Negocio/NegocioProvincia.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using DAO;
+using System.Data;
+
+namespace Negocio
+{
+    public class NegocioProvincia
+    {
+        public DataTable getTabla()
+        {
+            DaoProvincia dao = new DaoProvincia();
+            return dao.getTablaProvincias();
+        }
+
+        public Provincia get(int id)
+        {
+            DaoProvincia dao = new DaoProvincia();
+            Provincia prov = new Provincia();
+            prov.setID(id);
+            return dao.getProvincia(prov);
+        }
+
+        public bool eliminarProvincia(int id)
+        {
+            DaoProvincia dao = new DaoProvincia();
+            Provincia prov = new Provincia();
+            prov.setID(id);
+            // Verifico si el ID existe en la Base de Datos:
+            if (!dao.existeProvincia(prov))
+            {
+                int filasAfectadas = dao.eliminarProvincia(prov);
+                if (filasAfectadas == 1) { return true; }
+                else { return false; }
+            }
+            else { return false; }
+        }
+
+        public bool agregarProvincia(int id)
+        {
+            DaoProvincia dao = new DaoProvincia();
+            Provincia prov = new Provincia();
+            prov.setID(id);
+            // Verifico si el ID existe en la Base de Datos:
+            if (!dao.existeProvincia(prov))
+            {
+                int filasAfectadas = dao.agregarProvincia(prov);
+                if (filasAfectadas == 1) { return true; }
+                else { return false; }
+            }
+            else { return false; }
+        }
+    }
+}
---
+++ Negocio/NegocioSucursal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using DAO;
+using System.Data;
+
+namespace Negocio
+{
+    public class NegocioSucursal
+    {
+        public DataTable getTabla()
+        {
+            DaoSucursal dao = new DaoSucursal();
+            return dao.getTablaSucursales();
+        }
+
+        public Sucursal get(int id)
+        {
+            DaoSucursal dao = new DaoSucursal();
+            Sucursal suc = new Sucursal();
+            suc.setID(id);
+            return dao.getSucursal(suc);
+        }
+
+        public bool eliminarSucursal(int id)
+        {
+            DaoSucursal dao = new DaoSucursal();
+            Sucursal suc = new Sucursal();
+            suc.setID(id);
+            // Verifico si el ID existe en la Base de Datos:
+            if (!dao.existeSucursal(suc))
+            {
+                int filasAfectadas = dao.eliminarSucursal(suc);
+                if (filasAfectadas == 1) { return true; }
+                else { return false; }
+            }
+            else { return false; }
+        }
+
+        public bool agregarSucursal(String nombre)
+        {
+            DaoSucursal dao = new DaoSucursal();
+            Sucursal suc = new Sucursal();
+            suc.setNombre(nombre);
+            // Verifico si el ID existe en la Base de Datos:
+            if (!dao.existeSucursal(suc))
+            {
+                int filasAfectadas = dao.agregarSucursal(suc);
+                if (filasAfectadas == 1) { return true; }
+                else { return false; }
+            }
+            else { return false;  }
+        }
+    }
+}
